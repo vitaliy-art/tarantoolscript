d85f347ea81d1316dfee9bbc9703350d1d3b6873
Vitaliy Artemov <olivera507224@yandex.ru>
2023-12-26 20:30:14 +0000

    rocks (luatest): refactor Group declaration

--- src/rocks/luatest/Group.d.ts
+++ src/rocks/luatest/Group.d.ts
@@ -1,11 +1,12 @@
 declare module 'luatest' {
 
   export interface Group {
-    [key: string]: { (): void };
-    before_each:
-      | { (this: void, func: { (cg: ConfigGroup): void }): void }
-      | { (this: void, params: AnyTable, func: { (cg: ConfigGroup): void }): void };
-    before_test: { (name: string, params: AnyTable, func: { (cg: ConfigGroup): void }): void };
+    [key: string]: () => void;
+    before_each: {
+      (this: void, func: { (cg: ConfigGroup): void }): void;
+      (this: void, params: AnyTable, func: { (cg: ConfigGroup): void }): void;
+    }
+    before_test(name: string, params: AnyTable, func: { (cg: ConfigGroup): void }): void;
   }
 
   export interface ConfigGroup extends AnyTable {
