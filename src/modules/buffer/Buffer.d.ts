cff50700cc888a958320404801b78eafbbe5cc39
Vitaliy Artemov <olivera507224@yandex.ru>
2024-01-04 16:45:05 +0000

    buffer: fix declarations

--- src/modules/buffer/Buffer.d.ts
+++
@@ -1,8 +0,0 @@
-/** @noSelfInFile */
-
-import { BufferObject } from './BufferObject';
-
-declare module 'buffer' {
-  /** Create a new buffer. */
-  export function ibuf(): BufferObject;
-}
--- src/modules/buffer/index.d.ts
+++ src/modules/buffer/index.d.ts
@@ -1,2 +1,8 @@
-export * from './Buffer';
-export * from './BufferObject';
+/** @noSelfInFile */
+
+import { BufferObject } from './BufferObject';
+
+declare module 'buffer' {
+  /** Create a new buffer. */
+  export function ibuf(): BufferObject;
+}
