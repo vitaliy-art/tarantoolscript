c1997466b146155d1bfc4c71bdbe1015a6d651ff
Vitaliy Artemov <olivera507224@yandex.ru>
2024-01-22 16:28:36 +0000

    box (space): fix TriggerFunction type declaration

--- src/builtin/box/space/TriggerFunction.d.ts
+++ src/builtin/box/space/TriggerFunction.d.ts
@@ -1,9 +1,8 @@
 import { TupleObject } from '../tuple/TupleObject';
 
-export interface TriggerFunction {
-  (): TupleObject | void;
-  (old_tuple: TupleObject): TupleObject | void;
-  (old_tuple: TupleObject, new_tuple: TupleObject): TupleObject | void;
-  (old_tuple: TupleObject, new_tuple: TupleObject, space_name: string): TupleObject | void;
-  (old_tuple: TupleObject, new_tuple: TupleObject, space_name: string, request_type: 'INSERT' | 'DELETE' | 'UPDATE' | 'REPLACE'): TupleObject | void;
-}
+export type TriggerFunction =
+  | ((this: void) => TupleObject | void)
+  | ((this: void, old_tuple: TupleObject) => TupleObject | void)
+  | ((this: void, old_tuple: TupleObject, new_tuple: TupleObject) => TupleObject | void)
+  | ((this: void, old_tuple: TupleObject, new_tuple: TupleObject, space_name: string) => TupleObject | void)
+  | ((this: void, old_tuple: TupleObject, new_tuple: TupleObject, space_name: string, request_type: 'INSERT' | 'DELETE' | 'UPDATE' | 'REPLACE') => TupleObject | void)
