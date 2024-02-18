9cff4ed32dfed164000af416068e9f32a7e7bd2d
Vitaliy Artemov <olivera507224@yandex.ru>
2024-02-18 13:54:55 +0000

    add todos

--- src/builtin/index.d.ts
+++ src/builtin/index.d.ts
@@ -1,3 +1,43 @@
+/** @todo checks https://www.tarantool.io/en/doc/latest/reference/reference_lua/checks/ */
+/** @todo clock https://www.tarantool.io/en/doc/latest/reference/reference_lua/clock/ */
+/** @todo compat https://www.tarantool.io/en/doc/latest/reference/reference_lua/compat/ */
+/** @todo config https://www.tarantool.io/en/doc/latest/reference/reference_lua/config/ */
+/** @todo console https://www.tarantool.io/en/doc/latest/reference/reference_lua/console/ */
+/** @todo crypto https://www.tarantool.io/en/doc/latest/reference/reference_lua/crypto/ */
+/** @todo csv https://www.tarantool.io/en/doc/latest/reference/reference_lua/csv/ */
+/** @todo datetime https://www.tarantool.io/en/doc/latest/reference/reference_lua/datetime/ */
+/** @todo decimal https://www.tarantool.io/en/doc/latest/reference/reference_lua/decimal/ */
+/** @todo digest https://www.tarantool.io/en/doc/latest/reference/reference_lua/digest/ */
+/** @todo errno https://www.tarantool.io/en/doc/latest/reference/reference_lua/errno/ */
+/** @todo fiber https://www.tarantool.io/en/doc/latest/reference/reference_lua/errno/ */
+/** @todo fio https://www.tarantool.io/en/doc/latest/reference/reference_lua/fio/ */
+/** @todo fun https://www.tarantool.io/en/doc/latest/reference/reference_lua/fun/ */
+/** @todo http https://www.tarantool.io/en/doc/latest/reference/reference_lua/http/ */
+/** @todo iconv https://www.tarantool.io/en/doc/latest/reference/reference_lua/iconv/ */
+/** @todo jit https://www.tarantool.io/en/doc/latest/reference/reference_lua/jit/ */
+/** @todo json https://www.tarantool.io/en/doc/latest/reference/reference_lua/json/ */
+/** @todo key_def https://www.tarantool.io/en/doc/latest/reference/reference_lua/key_def/ */
+/** @todo log https://www.tarantool.io/en/doc/latest/reference/reference_lua/log/ */
+/** @todo merger https://www.tarantool.io/en/doc/latest/reference/reference_lua/merger/ */
+/** @todo net.box https://www.tarantool.io/en/doc/latest/reference/reference_lua/net_box/ */
+/** @todo os https://www.tarantool.io/en/doc/latest/reference/reference_lua/osmodule/ */
+/** @todo pickle https://www.tarantool.io/en/doc/latest/reference/reference_lua/pickle/ */
+/** @todo popen https://www.tarantool.io/en/doc/latest/reference/reference_lua/pickle/ */
+/** @todo socket https://www.tarantool.io/en/doc/latest/reference/reference_lua/socket/ */
+/** @todo strict https://www.tarantool.io/en/doc/latest/reference/reference_lua/strict/ */
+/** @todo string https://www.tarantool.io/en/doc/latest/reference/reference_lua/string/ */
+/** @todo swim https://www.tarantool.io/en/doc/latest/reference/reference_lua/swim/ */
+/** @todo table https://www.tarantool.io/en/doc/latest/reference/reference_lua/table/ */
+/** @todo tap https://www.tarantool.io/en/doc/latest/reference/reference_lua/tap/ */
+/** @todo tarantool https://www.tarantool.io/en/doc/latest/reference/reference_lua/tarantool/ */
+/** @todo uuid https://www.tarantool.io/en/doc/latest/reference/reference_lua/uuid/ */
+/** @todo utf8 https://www.tarantool.io/en/doc/latest/reference/reference_lua/utf8/ */
+/** @todo uri https://www.tarantool.io/en/doc/latest/reference/reference_lua/uri/ */
+/** @todo xlog https://www.tarantool.io/en/doc/latest/reference/reference_lua/xlog/ */
+/** @todo yaml https://www.tarantool.io/en/doc/latest/reference/reference_lua/yaml/ */
+/** @todo debug https://www.tarantool.io/en/doc/latest/reference/reference_lua/debug_facilities/ */
+
+
 export * from './box';
 export * from './buffer';
 export * from './msgpack';
--- src/rocks/index.d.ts
+++ src/rocks/index.d.ts
@@ -1,1 +1,17 @@
+/** @todo metrics https://www.tarantool.io/en/doc/latest/book/monitoring/ */
+/** @todo vshard https://www.tarantool.io/en/doc/latest/reference/reference_rock/vshard/ */
+/** @todo sql_dbms https://www.tarantool.io/en/doc/latest/reference/reference_rock/dbms/ */
+/** @todo crud https://github.com/tarantool/crud#readme */
+/** @todo expirationd https://tarantool.github.io/expirationd/ */
+/** @todo icu-date https://github.com/tarantool/icu-date#readme */
+/** @todo tracing https://github.com/tarantool/tracing/ */
+/** @todo avro-schema https://github.com/tarantool/avro-schema/blob/master/README.md */
+/** @todo http https://github.com/tarantool/http/blob/master/README.md */
+/** @todo icu-date https://github.com/tarantool/http/blob/master/README.md */
+/** @todo kafka https://github.com/tarantool/kafka/blob/master/README.md */
+/** @todo luacheck https://github.com/tarantool/luacheck?tab=readme-ov-file */
+/** @todo luarapidxml https://github.com/tarantool/luarapidxml/blob/master/README.md */
+/** @todo membership https://www.tarantool.io/en/doc/latest/reference/reference_rock/membership/#membership */
+
+
 export * as luatest from './luatest';
