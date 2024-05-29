5276bab5b8f65d304a148b4fc4406526a582786b
Vitaliy Artemov <olivera507224@yandex.ru>
2024-05-30 00:12:59 +0300

    tarantool: add module definition

---
+++ src/builtin/tarantool/Tarantool.d.ts
@@ -0,0 +1,24 @@
+/** @noSelfInFile */
+
+export declare const version: string;
+
+export declare namespace build {
+  export declare const target: string;
+  export declare const options: string;
+  export declare const linking: string;
+  export declare const mod_format: string;
+  export declare const flags: string;
+  export declare const compiler: string;
+  export declare const test_build: boolean;
+  export declare const asan: boolean;
+}
+
+export declare const package: string;
+
+export function pid(): number;
+
+export function uptime(): number;
+
+export declare namespace debug {
+  export function getsources(moduleName: string): string?;
+}
