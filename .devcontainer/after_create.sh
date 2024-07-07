b588bf87085155bace25ff49c15b784d2634fac0
Vitaliy Artemov <olivera507224@yandex.ru>
2024-07-08 01:00:16 +0300

    vscode: add devcontainer config

---
+++ .devcontainer/after_create.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+npm install
+
+curl -L https://tarantool.io/knVRRof/release/3/installer.sh | bash
+sudo apt-get -y install tarantool tt
+apt-get install -y tarantool-dev cmake
+tt rocks install --tree ./build/.rocks http
+tt rocks install --tree ./build/.rocks luatest
+tt rocks install --tree ./build/.rocks metrics
+tt rocks install --tree ./build/.rocks queue
