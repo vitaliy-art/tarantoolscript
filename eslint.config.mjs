6b40978081da96c71167c9f68f516de8d4e77ba3
Vitaliy Artemov <olivera507224@yandex.ru>
2024-09-10 12:20:05 +0300

    migrate eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsdoc from "eslint-plugin-tsdoc";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [
+    ...compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
+    {
+        plugins: {
+            "@typescript-eslint": typescriptEslint,
+            tsdoc,
+        },
+
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
+
+            parser: tsParser,
+            ecmaVersion: "latest",
+            sourceType: "module",
+        },
+
+        rules: {
+            indent: ["error", 2],
+            "linebreak-style": ["error", "unix"],
+            quotes: ["error", "single"],
+            semi: ["error", "always"],
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-var-requires": "off",
+        },
+    },
+];
