c5427cae00b5ba0657389883b532b78b579242f2
Vitaliy Artemov <olivera507224@yandex.ru>
2024-11-05 23:22:28 +0300

    fun: add exports

--- src/builtin/index.d.ts
+++ src/builtin/index.d.ts
@@ -3,7 +3,6 @@
 /** @todo console https://www.tarantool.io/en/doc/latest/reference/reference_lua/console/ */
 /** @todo crypto https://www.tarantool.io/en/doc/latest/reference/reference_lua/crypto/ */
 /** @todo errno https://www.tarantool.io/en/doc/latest/reference/reference_lua/errno/ */
-/** @todo fun https://www.tarantool.io/en/doc/latest/reference/reference_lua/fun/ */
 /** @todo jit https://www.tarantool.io/en/doc/latest/reference/reference_lua/jit/ */
 /** @todo merger https://www.tarantool.io/en/doc/latest/reference/reference_lua/merger/ */
 /** @todo pickle https://www.tarantool.io/en/doc/latest/reference/reference_lua/pickle/ */
@@ -13,23 +12,23 @@
 /** @todo tap https://www.tarantool.io/en/doc/latest/reference/reference_lua/tap/ */
 /** @todo debug https://www.tarantool.io/en/doc/latest/reference/reference_lua/debug_facilities/ */
 
-
-export * from './box';
-export * from './buffer';
-export * from './checks';
-export * from './csv';
-export * from './datetime';
-export * from './fiber';
-export * from './fio';
-export * from './http.client';
-export * from './json';
-export * from './key_def';
-export * from './log';
-export * from './msgpack';
-export * from './net.box';
-export * from './package';
-export * from './uri';
-export * from './utf8';
-export * from './uuid';
-export * from './yaml';
-export * from './extra';
+export * from "./box";
+export * from "./buffer";
+export * from "./checks";
+export * from "./csv";
+export * from "./datetime";
+export * from "./fiber";
+export * from "./fio";
+export * from "./fun";
+export * from "./http.client";
+export * from "./json";
+export * from "./key_def";
+export * from "./log";
+export * from "./msgpack";
+export * from "./net.box";
+export * from "./package";
+export * from "./uri";
+export * from "./utf8";
+export * from "./uuid";
+export * from "./yaml";
+export * from "./extra";
