d97922e7cb80ce085c2ec71da586aaf5817a682f
Vitaliy Artemov <olivera507224@yandex.ru>
2024-12-22 21:48:38 +0300

    builtin (crypto): add declarations

---
+++ src/builtin/crypto/Crypto.d.ts
@@ -0,0 +1,198 @@
+/** @noSelfInFile */
+
+/**
+ * Pass or return a cipher derived from the string, key, and (optionally, sometimes) initialization vector.
+ */
+export declare namespace cipher {
+  /**
+   * aes-128 (with 192-bit binary strings using AES).
+   */
+  export declare const aes128: CipherAlgorithm;
+
+  /**
+   * aes-192 (with 192-bit binary strings using AES).
+   */
+  export declare const aes192: CipherAlgorithm;
+
+  /**
+   * aes-256 (with 256-bit binary strings using AES).
+   */
+  export declare const aes256: CipherAlgorithm;
+
+  /**
+   * des (with 56-bit binary strings using DES, though DES is not recommended).
+   */
+  export declare const des: CipherAlgorithm;
+}
+
+declare interface CipherAlgorithm {
+  /**
+   * Cipher Block Chaining.
+   */
+  cbc: CipherMode;
+
+  /**
+   * Cipher Feedback.
+   */
+  cfb: CipherMode;
+
+  /**
+   * Electronic Codebook.
+   */
+  ecb: CipherMode;
+
+  /**
+   * Output Feedback.
+   */
+  ofb: CipherMode;
+}
+
+declare interface CipherMode {
+  encrypt(s: string, key: string, initializationVector: string): string;
+  decrypt(s: string, key: string, initializationVector: string): string;
+}
+
+/**
+ * Pass or return a digest derived from the string.
+ */
+export declare namespace digest {
+  /**
+   * dss (using DSS).
+   */
+  export declare function dss(s: string): string;
+
+  /**
+   * dss (using DSS-1).
+   */
+  export declare function dss1(s: string): string;
+
+  /**
+   * md4 (with 128-bit binary strings using MD4).
+   */
+  export declare function md4(s: string): string;
+
+  /**
+   * md5 (with 128-bit binary strings using MD5).
+   */
+  export declare function md5(s: string): string;
+
+  /**
+   * mdc2 (using MDC2).
+   */
+  export declare function mdc2(s: string): string;
+
+  /**
+   * ripemd (with 160-bit binary strings using RIPEMD-160).
+   */
+  export declare function ripemd160(s: string): string;
+
+  /**
+   * sha-1 (with 160-bit binary strings using SHA-1).
+   */
+  export declare function sha1(s: string): string;
+
+  /**
+   * sha-224 (with 224-bit binary strings using SHA-2).
+   */
+  export declare function sha224(s: string): string;
+
+  /**
+   * sha-256 (with 256-bit binary strings using SHA-2).
+   */
+  export declare function sha256(s: string): string;
+
+  /**
+   * sha-384 (with 384-bit binary strings using SHA-2).
+   */
+  export declare function sha384(s: string): string;
+
+  /**
+   * sha-512(with 512-bit binary strings using SHA-2)..
+   */
+  export declare function sha512(s: string): string;
+}
+
+/**
+ * Pass a key and a string. The result is an HMAC message authentication code.
+ */
+export declare namespace hmac {
+  /**
+   * md4 (with 128-bit binary strings using MD4).
+   */
+  export declare function md4(key: string, s: string): string;
+
+  /**
+   * md4 (with 128-bit binary strings using MD4).
+   */
+  export declare function md4_hex(key: string, s: string): string;
+
+  /**
+   * md5 (with 128-bit binary strings using MD5).
+   */
+  export declare function md5(key: string, s: string): string;
+
+  /**
+   * md5 (with 128-bit binary strings using MD5).
+   */
+  export declare function md5_hex(key: string, s: string): string;
+
+  /**
+   * ripemd (with 160-bit binary strings using RIPEMD-160).
+   */
+  export declare function ripemd160(key: string, s: string): string;
+
+  /**
+   * ripemd (with 160-bit binary strings using RIPEMD-160).
+   */
+  export declare function ripemd160_hex(key: string, s: string): string;
+
+  /**
+   * sha-1 (with 160-bit binary strings using SHA-1).
+   */
+  export declare function sha1(key: string, s: string): string;
+
+  /**
+   * sha-1 (with 160-bit binary strings using SHA-1).
+   */
+  export declare function sha1_hex(key: string, s: string): string;
+
+  /**
+   * sha-224 (with 224-bit binary strings using SHA-2).
+   */
+  export declare function sha224(key: string, s: string): string;
+
+  /**
+   * sha-224 (with 224-bit binary strings using SHA-2).
+   */
+  export declare function sha224_hex(key: string, s: string): string;
+
+  /**
+   * sha-256 (with 256-bit binary strings using SHA-2).
+   */
+  export declare function sha256(key: string, s: string): string;
+
+  /**
+   * sha-256 (with 256-bit binary strings using SHA-2).
+   */
+  export declare function sha256_hex(key: string, s: string): string;
+
+  /**
+   * sha-384 (with 384-bit binary strings using SHA-2).
+   */
+  export declare function sha384(key: string, s: string): string;
+
+  /**
+   * sha-384 (with 384-bit binary strings using SHA-2).
+   */
+  export declare function sha384_hex(key: string, s: string): string;
+
+  /**
+   * sha-512(with 512-bit binary strings using SHA-2).
+   */
+  export declare function sha512(key: string, s: string): string;
+
+  /**
+   * sha-512(with 512-bit binary strings using SHA-2).
+   */
+  export declare function sha512_hex(key: string, s: string): string;
+}
--- src/builtin/index.d.ts
+++ src/builtin/index.d.ts
@@ -1,7 +1,6 @@
 /** @todo compat https://www.tarantool.io/en/doc/latest/reference/reference_lua/compat/ */
 /** @todo config https://www.tarantool.io/en/doc/latest/reference/reference_lua/config/ */
 /** @todo console https://www.tarantool.io/en/doc/latest/reference/reference_lua/console/ */
-/** @todo crypto https://www.tarantool.io/en/doc/latest/reference/reference_lua/crypto/ */
 /** @todo errno https://www.tarantool.io/en/doc/latest/reference/reference_lua/errno/ */
 /** @todo jit https://www.tarantool.io/en/doc/latest/reference/reference_lua/jit/ */
 /** @todo merger https://www.tarantool.io/en/doc/latest/reference/reference_lua/merger/ */
@@ -12,23 +11,23 @@
 /** @todo tap https://www.tarantool.io/en/doc/latest/reference/reference_lua/tap/ */
 /** @todo debug https://www.tarantool.io/en/doc/latest/reference/reference_lua/debug_facilities/ */
 
-export * from "./box";
-export * from "./buffer";
-export * from "./checks";
-export * from "./csv";
-export * from "./datetime";
-export * from "./fiber";
-export * from "./fio";
-export * from "./fun";
-export * from "./http.client";
-export * from "./json";
-export * from "./key_def";
-export * from "./log";
-export * from "./msgpack";
-export * from "./net.box";
-export * from "./package";
-export * from "./uri";
-export * from "./utf8";
-export * from "./uuid";
-export * from "./yaml";
-export * from "./extra";
+export * from './box';
+export * from './buffer';
+export * from './checks';
+export * from './csv';
+export * from './datetime';
+export * from './fiber';
+export * from './fio';
+export * from './fun';
+export * from './http.client';
+export * from './json';
+export * from './key_def';
+export * from './log';
+export * from './msgpack';
+export * from './net.box';
+export * from './package';
+export * from './uri';
+export * from './utf8';
+export * from './uuid';
+export * from './yaml';
+export * from './extra';
