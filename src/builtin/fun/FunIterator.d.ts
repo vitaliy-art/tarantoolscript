e5b7a278b4cd8c81cbfdb14b33ff65c1f55df01c
Artemov Vitaliy <olivera507224@yandex.ru>
2025-08-27 01:39:14 +0300

    builtin (fun): fix FunIterator tomap method return type

--- src/builtin/fun/FunIterator.d.ts
+++ src/builtin/fun/FunIterator.d.ts
@@ -335,7 +335,13 @@ export declare type FunIterator<
    * @returns A new table (map) from iterated values.
    * @see {@link https://luafun.github.io/reducing.html#fun.tomap}
    */
-  tomap(): TReturn extends string | unknown[] ? [] : AnyTable;
+  tomap(): TReturn extends string | [unknown]
+    ? []
+    : TReturn extends [infer TFirst, infer TSecond, ...unknown[]]
+    ? TFirst extends AnyNotNil
+      ? LuaTable<TFirst, TSecond>
+      : never
+    : never;
 
   /**
    * Returns `true` if the iterator is a prefix of the `right`.
